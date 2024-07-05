193cb404f11a8fbd6a8389e5ea62a3d9e0d8fcbd
Alejandro Echavarria <aechavarriaj@gmail.com>
2024-07-05 08:59:15 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,56 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const containerImg = document.querySelector(".container-img");
+document.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield getAllCharacters();
+    data.forEach((character) => {
+        console.log(character);
+        // Crear contenedor principal para cada personaje
+        const characterCont = document.createElement("div");
+        characterCont.classList.add("character-cont");
+        // Crear contenedor para la imagen y la información del personaje
+        const backgroundCont = document.createElement("div");
+        const imagenCont = document.createElement("div");
+        const informacion = document.createElement("div");
+        backgroundCont.classList.add("backgroundCont");
+        imagenCont.classList.add("imagenCont");
+        informacion.classList.add("informacion");
+        // Crear y asignar elementos para la imagen y la información del personaje
+        const name = document.createElement("h4");
+        const image = document.createElement("img");
+        const ki = document.createElement("p");
+        const maxki = document.createElement("p");
+        const raza = document.createElement("p");
+        name.innerText = `Nombre: ${character.name}`;
+        image.src = character.image;
+        image.className = "image";
+        ki.innerText = `Ki: ${character.ki}`;
+        maxki.innerText = `MaxKi: ${character.maxKi}`;
+        raza.innerText = `Raza: ${character.race}`;
+        // Añadir elementos a los contenedores correspondientes
+        imagenCont.appendChild(image);
+        informacion.appendChild(name);
+        informacion.appendChild(ki);
+        informacion.appendChild(maxki);
+        informacion.appendChild(raza);
+        // Añadir contenedores al contenedor principal del personaje
+        characterCont.appendChild(backgroundCont);
+        backgroundCont.appendChild(imagenCont);
+        characterCont.appendChild(informacion);
+        // Añadir contenedor principal del personaje al contenedor principal de la página
+        containerImg.appendChild(characterCont);
+    });
+}));
+const getAllCharacters = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("https://dragonball-api.com/api/characters");
+    let data = yield response.json();
+    return data.items;
+});
+export {};
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import {
+    Welcome,
+    Item,
+} from "./interfaces/dragonBallZInterface.js";
+
+const containerImg = document.querySelector(".container-img") as HTMLDivElement;
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const data: Item[] = await getAllCharacters();
+
+    data.forEach((character: Item) => {
+        console.log(character);
+
+        // Crear contenedor principal para cada personaje
+        const characterCont = document.createElement("div") as HTMLDivElement;
+        characterCont.classList.add("character-cont");
+
+        // Crear contenedor para la imagen y la información del personaje
+        const backgroundCont = document.createElement("div") as HTMLDivElement;
+        const imagenCont = document.createElement("div") as HTMLDivElement;
+        const informacion = document.createElement("div") as HTMLDivElement;
+        backgroundCont.classList.add("backgroundCont");
+        imagenCont.classList.add("imagenCont");
+        informacion.classList.add("informacion");
+
+        // Crear y asignar elementos para la imagen y la información del personaje
+        const name = document.createElement("h4") as HTMLHeadingElement;
+        const image = document.createElement("img") as HTMLImageElement;
+        const ki = document.createElement("p") as HTMLParagraphElement;
+        const maxki = document.createElement("p") as HTMLParagraphElement;
+        const raza = document.createElement("p") as HTMLParagraphElement;
+
+        name.innerText = `Nombre: ${character.name}`;
+        image.src = character.image;
+        image.className = "image";
+        ki.innerText = `Ki: ${character.ki}`;
+        maxki.innerText = `MaxKi: ${character.maxKi}`;
+        raza.innerText = `Raza: ${character.race}`;
+
+        // Añadir elementos a los contenedores correspondientes
+        imagenCont.appendChild(image);
+        informacion.appendChild(name);
+        informacion.appendChild(ki);
+        informacion.appendChild(maxki);
+        informacion.appendChild(raza);
+
+        // Añadir contenedores al contenedor principal del personaje
+        characterCont.appendChild(backgroundCont);
+        backgroundCont.appendChild(imagenCont)
+        characterCont.appendChild(informacion);
+
+        // Añadir contenedor principal del personaje al contenedor principal de la página
+        containerImg.appendChild(characterCont);
+    });
+});
+
+const getAllCharacters = async (): Promise<Item[]> => {
+    const response = await fetch("https://dragonball-api.com/api/characters");
+    let data: Welcome = await response.json();
+
+    return data.items;
+};
